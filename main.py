40074cbedbdc486846aea66b72f4759ad6ad4684
AlexForty <alexforty@sky.com>
2024-03-19 15:42:23 +0000

    make rest api

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 # Write a rock, paper, scissors game that is played against the computer.
+from flask import Flask, request, jsonify
 # import random module
 import random
+
+app = Flask(__name__)
+
 # define main function that handles all the logic
 def determine_winner(user_choice, computer_choice):
     valid_choices = ['rock', 'paper', 'scissors', 'lizard', 'spock']
@@ -17,8 +21,12 @@ def determine_winner(user_choice, computer_choice):
     else:
         return 'You lose!'
 
-def main():
-    choices = ['rock', 'paper', 'scissors', 'lizard', 'spock']
-    user_choice = input("Enter your choice (rock, paper, scissors, lizard, spock): ")
-    computer_choice = random.choice(choices)
-    print(determine_winner(user_choice, computer_choice))
+@app.route('/play', methods=['POST'])
+def play():
+    user_choice = request.json.get('user_choice')
+    computer_choice = random.choice(['rock', 'paper', 'scissors', 'lizard', 'spock'])
+    result = determine_winner(user_choice, computer_choice)
+    return jsonify({'result': result})
+
+if __name__ == '__main__':
+    app.run(debug=True)
